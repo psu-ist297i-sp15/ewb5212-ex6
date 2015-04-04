5a427727ca2f5028901ac370a42c00faa1c0cf38
Dan Coughlin <dan.coughlin@gmail.com>
2015-04-04 12:20:58 -0400

    added the votes model, it was missing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150404160123) do
+ActiveRecord::Schema.define(version: 20150404161901) do
 
   create_table "topics", force: :cascade do |t|
     t.string   "title"
@@ -37,5 +37,11 @@ ActiveRecord::Schema.define(version: 20150404160123) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "topic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
